eab9d3e5fab33437fc358bc72dee8d57c2224696
Joao Fernandes <joaofergear003@gmail.com>
2022-12-12 14:42:34 -0300

    build: insert svg files in project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { GlobalStyles } from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
 export const App = () => {
   return (
-    <>Hello world</>
+  	<>
+  		<ThemeProvider theme={defaultTheme}>
+  			<GlobalStyles />
+   		<DefaultLayout />
+   	</ThemeProvider>
+    </>
   )
 }
