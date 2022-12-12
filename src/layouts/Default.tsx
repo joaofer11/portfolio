599480141bbaeb465bc3281b8b989f1a6f91ab98
Joao Fernandes <joaofergear003@gmail.com>
2022-12-12 13:48:33 -0300

    chore: create DefaultLayout component for app-wide

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Layout = styled.div`
+	padding: 0.714rem 1.428rem;
+`
+
+export const Default = () => {
+	return (
+		<Layout>
+			...
+		</Layout>
+	)
+}
