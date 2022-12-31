15592bb45602d0f1d176a0d2662e18ef42951b02
Joao Fernandes <joaofergear003@gmail.com>
2022-12-31 14:17:02 -0300

    feat(projects-page): add intro section

--- src/features/portfolio/pages/Projects/index.tsx
+++ src/features/portfolio/pages/Projects/index.tsx
@@ -1,18 +1,25 @@
 import * as S from './styles'
 import { PROJECTS_DATA } from './data'
 import { ProjectCard } from './components/ProjectCard'
+import { SeparatorSvg } from '../../../../styles/svg-components/SeparatorSvg'
 
 export const Projects = () => {
 	return (
 		<S.Main>
-			<ul>
-				{PROJECTS_DATA.map(projectData => (
-					<ProjectCard 
-						key={projectData.id} 
-						projectData={projectData} 
-					/>
-				))}
-			</ul>
+			<section>
+				<h1>Confira alguns projetos que desenvolvi </h1>
+			</section>
+			<SeparatorSvg />
+			<section>
+				<ul>
+					{PROJECTS_DATA.map(projectData => (
+						<ProjectCard 
+							key={projectData.id} 
+							projectData={projectData} 
+						/>
+					))}
+				</ul>
+			</section>
 		</S.Main>
 	)
 }
--- src/features/portfolio/pages/Projects/styles.ts
+++ src/features/portfolio/pages/Projects/styles.ts
@@ -2,9 +2,30 @@ import styled from 'styled-components'
 
 export const Main = styled.main`
 	margin: 1.428rem 0;
-	padding: 0 1.428rem;
 	
-	> ul {
+	section {
+		padding: 0 1.428rem;
+	}
+	
+	> section:first-child {
+		> h1 {
+			font-weight: 700;
+			font-size: 1.714rem;
+			font-family: 'Ubuntu', sans-serif;
+			
+			line-height: 1.3;
+			text-transform: uppercase;
+			color: ${({ theme }) => theme['gray-blue-400']};
+			
+			&::after {
+				content: ".";
+				font-size: 3.428rem;
+				line-height: 0;
+			}
+		}
+	}
+	
+	ul {
 		display: flex;
 		flex-direction: column;
 		gap: 1.428rem;
