7079edcea583da75ea2aadd998966fc3473fc38e
Joao Fernandes <joaofergear003@gmail.com>
2023-01-01 13:08:00 -0300

    improvement(AppearingAnimation): add possibility to disable animation

--- src/common/components/AppearingAnimation/index.tsx
+++ src/common/components/AppearingAnimation/index.tsx
@@ -1,20 +1,22 @@
 import { ReactNode } from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 type TPosition = 'top' | 'right' | 'bottom' | 'left'
 
 interface IAppearingAnimationProps {
+	active: boolean;
 	children: ReactNode;
 	position: TPosition;
 }
 
 interface IS_AppearingAnimationProps {
 	position: TPosition;
+	active: boolean;
 }
 
 const animeAppearing = keyframes`
 	to {
-		transform: translate(0, 0);
+		transform: translate3d(0, 0, 0);
 		opacity: 1;
 	}
 `
@@ -23,28 +25,34 @@ const S_AppearingAnimation = styled.div<IS_AppearingAnimationProps>`
 	${({ position }) => {
 		switch (position) {
 			case 'top':
-				return 'transform: translateY(-12.5%);'
+				return 'transform: translate3d(0, -12.5%, 0);'
 			break;
 			case 'right':
-				return 'transform: translateX(12.5%);'
+				return 'transform: translate3d(12.5%, 0, 0);'
 			break;
 			case 'bottom':
-				return 'transform: translateY(12.5%);'
+				return 'transform: translate3d(0, 12.5%, 0);'
 			break;
 			case 'left':
-				return 'transform: translateX(-12.5%);'
+				return 'transform: translate3d(-12.5%, 0, 0);'
 			break;
 		}
 	}}
 	opacity: 0;
-	animation: ${animeAppearing} 800ms ease forwards;
+	${({ active }) => active ? css`
+		animation: ${animeAppearing} 800ms ease forwards;
+	`: ''};
 `
 
 export const AppearingAnimation = ({ 
+	active,
 	children,
 	position,
 }: IAppearingAnimationProps) => (
-	<S_AppearingAnimation position={position}>
+	<S_AppearingAnimation
+		active={active}
+		position={position}
+	>
 		{children}
 	</S_AppearingAnimation>
 )
