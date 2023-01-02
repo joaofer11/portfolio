af44c6fc6d87e5c84348fb1892618f4c623a22ef
Joao Fernandes <joaofergear003@gmail.com>
2023-01-02 17:35:27 -0300

    build: remove redux of the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
 import { App } from './app/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-	<Provider store={store}>
    	<App />
-   </Provider>
   </React.StrictMode>,
 )
