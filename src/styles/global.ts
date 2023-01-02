e7c0f369e8bf0da6bef17b127557324ee4dcaf99
Joao Fernandes <joaofergear003@gmail.com>
2023-01-01 22:40:46 -0300

    change(ui): change the root font size and implement css variables
    change from '14px' base font-size to '16px' and also implement some 'CSS custom properties' to help on future responsively design

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,6 +2,19 @@ import { createGlobalStyle } from 'styled-components'
 import { sizes } from './breakpoints'
 
 export const GlobalStyles = createGlobalStyle`
+	:root {
+		--font-size-xxs: 0.75rem;
+		--font-size-xs: 0.875rem;
+		--font-size-sm: 1rem;
+		--font-size-md: 1.125rem;
+		--font-size-lg: 1.3125rem;
+		
+		--spacing-xxs: 0.3125rem;
+		--spacing-xs: 0.625rem;
+		--spacing-sm: 1.25rem;
+		--spacing-md: 1.5625rem;
+	}
+	
 	* {
 		margin: 0;
 		padding: 0;
@@ -9,15 +22,7 @@ export const GlobalStyles = createGlobalStyle`
 	}
 	
 	html {
-		font-size: 87.5%;
-		
-		@media ${sizes.md} {
-			font-size: 93.75%;
-		}
-		
-		@media ${sizes.lg} {
-			font-size: 100%;
-		}
+		font-size: 100%;
 	}
 	
 	body {
