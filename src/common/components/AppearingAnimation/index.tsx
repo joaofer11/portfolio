d4076b434e19ce0f41ac7a80b13bd4ded6d9b73c
Joao Fernandes <joaofergear003@gmail.com>
2023-01-04 11:46:05 -0300

    refactor: refactor 'AppearingAnimation' component

--- src/common/components/AppearingAnimation/index.tsx
+++ src/common/components/AppearingAnimation/index.tsx
@@ -1,58 +1,17 @@
 import { ReactNode } from 'react'
-import styled, { css, keyframes } from 'styled-components'
+import * as S from './styles'
 
-type TPosition = 'top' | 'right' | 'bottom' | 'left'
-
-interface IAppearingAnimationProps {
+export interface IProps {
 	active: boolean;
 	children: ReactNode;
-	position: TPosition;
+	position: 'top' | 'right' | 'bottom' | 'left';
 }
 
-interface IS_AppearingAnimationProps {
-	position: TPosition;
-	active: boolean;
-}
-
-const animeAppearing = keyframes`
-	to {
-		transform: translate3d(0, 0, 0);
-		opacity: 1;
-	}
-`
-
-const S_AppearingAnimation = styled.div<IS_AppearingAnimationProps>`
-	${({ position }) => {
-		switch (position) {
-			case 'top':
-				return 'transform: translate3d(0, -15px, 0);'
-			break;
-			case 'right':
-				return 'transform: translate3d(12.5%, 0, 0);'
-			break;
-			case 'bottom':
-				return 'transform: translate3d(0, 15px, 0);'
-			break;
-			case 'left':
-				return 'transform: translate3d(-12.5%, 0, 0);'
-			break;
-		}
-	}}
-	opacity: 0;
-	${({ active }) => active ? css`
-		animation: ${animeAppearing} 800ms ease forwards;
-	`: ''};
-`
-
-export const AppearingAnimation = ({ 
-	active,
-	children,
-	position,
-}: IAppearingAnimationProps) => (
-	<S_AppearingAnimation
+export const AppearingAnimation = ({ active, children, position }: IProps) => (
+	<S.AppearingAnimation
 		active={active}
 		position={position}
 	>
 		{children}
-	</S_AppearingAnimation>
+	</S.AppearingAnimation>
 )
---
+++ src/common/components/AppearingAnimation/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css, keyframes } from 'styled-components'
+import type { IProps } from './'
+
+const animeAppearing = keyframes`
+	to {
+		transform: translate3d(0, 0, 0);
+		opacity: 1;
+	}
+`
+
+type IStyledProps = Omit<IProps, 'children'>
+
+export const AppearingAnimation = styled.div<IStyledProps>`
+	${({ position }) => {
+		switch (position) {
+			case 'top':
+				return 'transform: translate3d(0, -15px, 0);'
+			break;
+			case 'right':
+				return 'transform: translate3d(12.5%, 0, 0);'
+			break;
+			case 'bottom':
+				return 'transform: translate3d(0, 15px, 0);'
+			break;
+			case 'left':
+				return 'transform: translate3d(-12.5%, 0, 0);'
+			break;
+		}
+	}}
+	opacity: 0;
+	${({ active }) => active ? css`
+		animation: ${animeAppearing} 800ms ease forwards;
+	`: ''};
+`
