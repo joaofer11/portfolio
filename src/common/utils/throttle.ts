6b489dbb14e38c7b57993945f23e8c6d0461edd9
Joao Fernandes <joaofergear003@gmail.com>
2023-01-04 11:51:46 -0300

    style(utils/throttle): change from args name convention to params

--- src/common/utils/throttle.ts
+++ src/common/utils/throttle.ts
@@ -1,19 +1,19 @@
-export const throttle = <Args>(fn: (...args: Args[]) => any, delay: number = 1000) => {
+export const throttle = <Params>(fn: (...Params: Params[]) => any, delay: number = 1000) => {
 	let shouldAwait = false
-	let pendingArgs: Args[] | null = null
+	let pendingParams: Params[] | null = null
 	
 	const timeoutFn = () => {
-		const hasPendingArgs = pendingArgs !== null
-		if (!hasPendingArgs) return shouldAwait = false
+		const hasPendingParams = pendingParams !== null
+		if (!hasPendingParams) return shouldAwait = false
 		
-		fn(...pendingArgs as Args[])
-		pendingArgs = null
+		fn(...pendingParams as Params[])
+		pendingParams = null
 		setTimeout(timeoutFn, delay)
 	}
 	
-	return (...args: Args[]) => {
-		if (shouldAwait) return pendingArgs = args
-		fn(...args)
+	return (...params: Params[]) => {
+		if (shouldAwait) return pendingParams = params
+		fn(...params)
 		shouldAwait = true
 		setTimeout(timeoutFn, delay)
 	}
