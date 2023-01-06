54d211b6582f07c54d5c74fa80f3049ec8630ed6
Joao Fernandes <joaofergear003@gmail.com>
2023-01-05 21:46:01 -0300

    improvement(ui): change some footer styles

--- src/features/ui/components/Footer/index.tsx
+++ src/features/ui/components/Footer/index.tsx
@@ -5,14 +5,42 @@ import { FaDiscord } from 'react-icons/fa';
 export const Footer = () => (
 	<S.Footer>
 		<p>
-			© 2022 - Feito com 💙 por João Fernandes
+         © 2022 - Feito com 💙 por <span>João Fernandes</span>
 		</p>
-		
+
 		<S.SocialNetworks>
-			<li><a href="mailto:joaofergear003@gmail.com" target="_blank" rel="noreferrer"><FiMail /></a></li>
-			<li><a href="#discord-nick" title="joaofer11#2087"><FaDiscord /></a></li>
-			<li><a href="https://github.com/joaofer11" target="_blank" rel="noreferrer"><i className="devicon-github-original"></i></a></li>
-			<li><a href="https://www.linkedin.com/in/jo%C3%A3o-fernandes-569461253/" target="_blank" rel="noreferrer"><i className="devicon-linkedin-plain"></i></a></li>
+			<li>
+				<a
+					href="mailto:joaofergear003@gmail.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiMail />
+				</a>
+			</li>
+			<li>
+				<a href="#discord-nick" title="joaofer11#2087">
+					<FaDiscord />
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://github.com/joaofer11"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="devicon-github-original"></i>
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://www.linkedin.com/in/jo%C3%A3o-fernandes-569461253/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="devicon-linkedin-plain"></i>
+				</a>
+			</li>
 		</S.SocialNetworks>
 	</S.Footer>
 );
--- src/features/ui/components/Footer/styles.ts
+++ src/features/ui/components/Footer/styles.ts
@@ -1,35 +1,41 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: var(--spacing-xs);
-	padding: var(--spacing-sm) 0;
-	
-	> p {
-		font-family: 'Ubuntu', sans-serif;
-		font-size: var(--font-size-xxs);
-		font-weight: 700;
-		line-height: 1.5;
-		
-		color: ${({ theme }) => theme['gray-blue-800']};
-	}
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   gap: var(--spacing-xs);
+   padding: var(--spacing-sm) 0;
+
+   > p {
+      font-family: "Ubuntu", sans-serif;
+      font-size: var(--font-size-xxs);
+      font-weight: 700;
+      line-height: 1.5;
+
+      color: ${({ theme }) => theme['gray-blue-800']};
+
+      > span {
+         color: ${({ theme }) => theme['gray-blue-400']};
+      }
+   }
 `;
 
 export const SocialNetworks = styled.ul`
-	display: flex;
-	align-content: baseline;
-	gap: var(--spacing-xs);
-	font-size: var(--font-size-sm);
-	
-	a {
-		color: ${({ theme }) => theme['gray-blue-850']};
-		transition: color 300ms ease;
-		
-		&:hover {
-			color: ${({ theme }) => theme['blue-350']};
-		}
-	}
+   display: flex;
+   align-content: baseline;
+   gap: var(--spacing-xs);
+   font-size: var(--font-size-sm);
+
+   a {
+      display: block;
+      color: ${({ theme }) => theme['gray-blue-850']};
+      transition: all 300ms ease;
+
+      &:hover {
+         transform: scale(1.15);
+         color: ${({ theme }) => theme['blue-350']};
+      }
+   }
 `;
