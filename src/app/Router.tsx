acaf128cc1466db09c9c07f05e2f0a555d046a72
Joao Fernandes <joaofergear003@gmail.com>
2023-01-06 10:25:04 -0300

    feat(ui): implement redirecting to home when the page is not found

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,13 +1,14 @@
 import { Home } from '../features/portfolio';
-import { Routes, Route } from 'react-router-dom';
 import { Projects } from '../features/portfolio';
 import { DefaultLayout } from '../layouts/DefaultLayout';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const Router = () => (
 	<Routes>
 		<Route path="/" element={<DefaultLayout />}>
 			<Route index element={<Home />} />
 			<Route path="projects" element={<Projects />} />
+			<Route path="*" element={<Navigate to="/" />} />
 		</Route>
 	</Routes>
 );
